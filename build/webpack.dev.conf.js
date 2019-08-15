d78aa870152935d45e00c99afc9896b5b4cfe713
xiaiyu <1141600195@qq.com>
2019-08-15 19:47:36 +0800

    增加https配置

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -28,6 +28,8 @@ const devWebpackConfig = merge(baseWebpackConfig, {
         { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
       ],
     },
+    //todo https
+    https:true,
     hot: true,
     contentBase: false, // since we use CopyWebpackPlugin.
     compress: true,
